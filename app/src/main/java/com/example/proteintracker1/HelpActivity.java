af630e6124562ec02e48995ec01bf1cdfbef027e
destasp <desta.siwi@si.ukdw.ac.id>
2019-09-06 19:30:17 +0700

    https://github.com/destasp/Layout.git

---
+++ app/src/main/java/com/example/proteintracker1/HelpActivity.java
@@ -0,0 +1,22 @@
+package com.example.proteintracker1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        TextView myTextView = new TextView(this);
+        myTextView.setText("some help text");
+        setContentView(myTextView);
+        Bundle b = getIntent().getExtras();
+        String helpText = b.getString("helpString");
+        myTextView.setText(helpText);
+        setContentView(myTextView);
+    }
+}
